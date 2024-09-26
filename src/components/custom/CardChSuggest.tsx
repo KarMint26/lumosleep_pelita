f59c3ed906c56a8431506aca8948c7e8ed709118
dodichakill <dodiherlangga91@gmail.com>
2024-09-27 00:45:56 +0700

    fix layout chatbot

--- src/components/custom/CardChSuggest.tsx
+++ src/components/custom/CardChSuggest.tsx
@@ -10,7 +10,7 @@ const CardChSuggest = ({ icon, text, onHandleClick }: cardsg) => {
   return (
     <React.Fragment>
       <div
-        className="card-suggest w-32 lg:w-full text-center h-fit flex justify-center items-center flex-col lg:flex-row gap-2 lg:gap-4 bg-background ring ring-slate-600 border hover:border-mainColor p-3 sm:p-4 rounded-xl transition duration-300 cursor-pointer"
+        className="card-suggest w-[9.2rem] lg:w-full text-center h-fit flex justify-center items-center flex-col lg:flex-row gap-2 lg:gap-4 bg-background ring ring-slate-600 border hover:border-mainColor p-3 sm:p-4 rounded-xl transition duration-300 cursor-pointer"
         onClick={onHandleClick}
       >
         {icon}
